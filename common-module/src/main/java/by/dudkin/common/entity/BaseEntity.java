52e8aac865f1635354a6cc66b492845be3e422ce
Alexander Dudkin <alexraddan@gmail.com>
2024-10-24 00:28:38 +0300

    feat: add common module for all common stuff

---
+++ common-module/src/main/java/by/dudkin/common/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package by.dudkin.common.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.io.Serializable;
+
+/**
+ * @author Alexander Dudkin
+ */
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
