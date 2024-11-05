e3ffd8b1e2fe3cda992bf41ccb14e90d4e826a96
Alexander Dudkin <alexraddan@gmail.com>
2024-11-06 01:24:33 +0300

    Build Fix

--- driver-service/src/test/java/by/dudkin/driver/SpringConfigTests.java
+++ driver-service/src/test/java/by/dudkin/driver/SpringConfigTests.java
@@ -2,10 +2,12 @@ package by.dudkin.driver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  * @author Alexander Dudkin
  */
+@ActiveProfiles("hsqldb")
 @SpringBootTest(classes = DriverServiceApplication.class)
 public class SpringConfigTests {
 
