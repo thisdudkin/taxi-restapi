0ee041b0658704175afba7e93f2b5b1df8073bd9
Alexander Dudkin <alexraddan@gmail.com>
2024-11-15 16:19:50 +0300

    Fix

--- rides-service/src/main/java/by/dudkin/rides/rest/dto/request/RideCompletionRequest.java
+++ rides-service/src/main/java/by/dudkin/rides/rest/dto/request/RideCompletionRequest.java
@@ -2,13 +2,10 @@ package by.dudkin.rides.rest.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 
 import java.io.Serializable;
 
 /**
  * @author Alexander Dudkin
  */
-public record RideCompletionRequest(@NotNull @Positive Long rideId,
-                                    @Min(1) @NotNull Integer rating
-) implements Serializable { }
+public record RideCompletionRequest(@Min(1) @NotNull Integer rating) implements Serializable { }
