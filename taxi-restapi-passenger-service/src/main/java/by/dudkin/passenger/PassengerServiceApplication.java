de0343cf295e3b978b2ad1b47ca60de3fc203371
Alexander Dudkin <alexraddan@gmail.com>
2024-11-27 11:36:29 +0300

    Initial work
    * refactor: pom structure

--- taxi-restapi-passenger-service/src/main/java/by/dudkin/passenger/PassengerServiceApplication.java
+++ taxi-restapi-passenger-service/src/main/java/by/dudkin/passenger/PassengerServiceApplication.java
@@ -2,12 +2,14 @@ package by.dudkin.passenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author Alexander Dudkin
  */
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PassengerServiceApplication {
 
