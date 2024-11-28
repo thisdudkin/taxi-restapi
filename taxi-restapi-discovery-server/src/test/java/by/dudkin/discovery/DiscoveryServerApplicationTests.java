39a99dde8cceebe3e2552cf3ac99c688634fb31a
Alexander Dudkin <152862359+thisdudkin@users.noreply.github.com>
2024-11-28 16:52:56 +0300

    86c14qk4q: Implement Discovery Server with Eureka for Microservices
    * Initial work
    
    * refactor: pom structure
    
    * CI Build Fix

---
+++ taxi-restapi-discovery-server/src/test/java/by/dudkin/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,16 @@
+package by.dudkin.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Alexander Dudkin
+ */
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- taxi-restapi-rides-service/src/main/java/by/dudkin/rides/RidesServiceApplication.java
+++ taxi-restapi-rides-service/src/main/java/by/dudkin/rides/RidesServiceApplication.java
@@ -2,12 +2,14 @@ package by.dudkin.rides;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author Alexander Dudkin
  */
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RidesServiceApplication {
 
