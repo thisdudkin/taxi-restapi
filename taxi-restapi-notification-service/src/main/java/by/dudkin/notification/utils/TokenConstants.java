c5af39c104d7259f0cbdc973d6e3b522b62fadf6
Alexander Dudkin <alexraddan@gmail.com>
2025-01-20 16:29:24 +0300

    Initial work

---
+++ taxi-restapi-notification-service/src/main/java/by/dudkin/notification/utils/TokenConstants.java
@@ -0,0 +1,15 @@
+package by.dudkin.notification.utils;
+
+/**
+ * @author Alexander Dudkin
+ */
+public final class TokenConstants {
+
+    private TokenConstants() {
+        throw new AssertionError();
+    }
+
+    public static final String USERNAME_CLAIM = "preferred_username";
+    public static final String USERNAME_CLAIM_EXPRESSION = "claims['preferred_username']";
+
+}
