46ace7be5c1242019c7c98084cdb092e5b47ab86
Alexander Dudkin <152862359+thisdudkin@users.noreply.github.com>
2025-01-28 17:24:08 +0300

    86c1tv2hb: Implement Authentication Service MockMvc Testing

---
+++ taxi-restapi-authentication-server/src/test/java/by/dudkin/auth/AuthenticationControllerTests.java
@@ -0,0 +1,139 @@
+package by.dudkin.auth;
+
+import by.dudkin.i18n.I18nUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.keycloak.representations.AccessTokenResponse;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Alexander Dudkin
+ */
+@Import(TestSecurityConfig.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(AuthenticationController.class)
+class AuthenticationControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private I18nUtils i18nUtils;
+
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    private static final String REGISTER_URI = "/api/auth/register";
+    private static final String REGISTER_ADMIN_URI = "/api/auth/register/admin";
+    private static final String LOGIN_URI = "/api/auth/login";
+    private static final String REFRESH_URI = "/api/auth/refresh";
+
+    @Test
+    void whenRegisterUser_thenStatus201() throws Exception {
+        RegistrationRequest request = new RegistrationRequest(
+                "testUsername",
+                "testEmail@email.com",
+                "verySecretPassword",
+                Role.ROLE_PASSENGER
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.post(REGISTER_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+
+        Mockito.verify(authenticationService).saveUser(Mockito.any(RegistrationRequest.class));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void whenRegisterAdmin_thenStatus201() throws Exception {
+        RegistrationRequest request = new RegistrationRequest(
+                "admin",
+                "admin@admin.com",
+                "adminPassword",
+                Role.ROLE_ADMIN
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.post(REGISTER_ADMIN_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+
+        Mockito.verify(authenticationService).saveAdmin(Mockito.any(RegistrationRequest.class));
+    }
+
+    @Test
+    void whenRegisterAdmin_thenStatus401() throws Exception {
+        RegistrationRequest request = new RegistrationRequest(
+                "admin",
+                "admin@admin.com",
+                "adminPassword",
+                Role.ROLE_ADMIN
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.post(REGISTER_ADMIN_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(roles = "PASSENGER")
+    void whenRegisterAdmin_thenStatus403() throws Exception {
+        RegistrationRequest request = new RegistrationRequest(
+                "admin",
+                "admin@admin.com",
+                "adminPassword",
+                Role.ROLE_ADMIN
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.post(REGISTER_ADMIN_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void whenLogin_thenStatus200() throws Exception {
+        LoginRequest request = new LoginRequest(
+                "username",
+                "password"
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.post(LOGIN_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+
+        Mockito.verify(authenticationService).login(Mockito.any(LoginRequest.class));
+    }
+
+    @Test
+    void whenRefreshToken_thenStatus200() throws Exception {
+        Mockito.when(authenticationService.refreshToken(Mockito.any(String.class))).thenReturn(new AccessTokenResponse());
+
+        mockMvc.perform(MockMvcRequestBuilders.post(REFRESH_URI)
+                        .param("token", RandomStringUtils.randomAlphabetic(256)))
+                .andExpect(status().isOk());
+
+        Mockito.verify(authenticationService).refreshToken(Mockito.any(String.class));
+    }
+
+}
---
+++ taxi-restapi-authentication-server/src/test/java/by/dudkin/auth/TestSecurityConfig.java
@@ -0,0 +1,90 @@
+package by.dudkin.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @author Alexander Dudkin
+ */
+@TestConfiguration
+@EnableWebSecurity
+@EnableMethodSecurity
+class TestSecurityConfig {
+
+    private final ObjectMapper objectMapper;
+
+    TestSecurityConfig(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Bean
+    public SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.oauth2ResourceServer((oauth2) -> oauth2
+            .jwt(Customizer.withDefaults())
+            .accessDeniedHandler(accessDeniedHandler())
+            .authenticationEntryPoint(authenticationEntryPoint())
+        );
+
+        http.authorizeHttpRequests(authz -> authz
+            .requestMatchers("/api/auth/login").permitAll()
+            .requestMatchers("/api/auth/refresh").permitAll()
+            .requestMatchers("/api/auth/register").permitAll()
+            .requestMatchers("/api/auth/register/admin").hasRole("ADMIN")
+            .anyRequest().authenticated()
+        );
+
+        return http.build();
+
+    }
+
+    @Bean
+    AccessDeniedHandler accessDeniedHandler() {
+        return ((request, response, accessDeniedException) -> {
+            ProblemDetail detail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, accessDeniedException.getMessage());
+
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setContentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE);
+
+            String json = objectMapper.writeValueAsString(detail);
+            response.getWriter().write(json);
+        });
+    }
+
+    @Bean
+    AuthenticationEntryPoint authenticationEntryPoint() {
+        return ((request, response, authException) -> {
+            ProblemDetail detail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, authException.getMessage());
+
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE);
+
+            String json = objectMapper.writeValueAsString(detail);
+            response.getWriter().write(json);
+        });
+    }
+
+}
